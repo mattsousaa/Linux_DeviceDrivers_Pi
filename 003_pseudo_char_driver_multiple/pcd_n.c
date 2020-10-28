97f7903b4f6499cb78ccd3833b1461e1b8ea845e
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-10-28 12:07:52 -0300

    Initialing pcdrv_private_data

--- 003_pseudo_char_driver_multiple/pcd_n.c
+++ 003_pseudo_char_driver_multiple/pcd_n.c
@@ -16,9 +16,9 @@
 #define MEM_SIZE_MAX_PCDEV4  512
 
 /* permission codes */
-#define RDONLY 0x01
-#define WRONLY 0x10
-#define RDWR   0x11
+#define RDONLY 0x01			/* Read only */
+#define WRONLY 0x10			/* Write only */
+#define RDWR   0x11			/* Read and Write */
 
 /* pseudo device's memory */
 char device_buffer_pcdev1[MEM_SIZE_MAX_PCDEV1];
@@ -42,6 +42,39 @@ struct pcdrv_private_data {
 	struct class *class_pcd;								/* holds the class pointer */
 	struct device *device_pcd;								/* holds the device pointer */
 	struct pcdev_private_data pcdev_data[NO_OF_DEVICES];
+};
+
+struct pcdrv_private_data pcdrv_data = {
+	.total_devices = NO_OF_DEVICES,
+	.pcdev_data = {
+		[0] = {
+			.buffer = device_buffer_pcdev1, 
+			.size = MEM_SIZE_MAX_PCDEV1,
+			.serial_number = "PCDEV1XYZ123",
+			.perm = RDONLY
+		}, 
+
+		[1] = {
+			.buffer = device_buffer_pcdev2, 
+			.size = MEM_SIZE_MAX_PCDEV2,
+			.serial_number = "PCDEV2XYZ123",
+			.perm = WRONLY 
+		},
+
+		[2] = {
+			.buffer = device_buffer_pcdev3, 
+			.size = MEM_SIZE_MAX_PCDEV3,
+			.serial_number = "PCDEV3XYZ123",
+			.perm = RDWR 
+		},
+
+		[3] = {
+			.buffer = device_buffer_pcdev4, 
+			.size = MEM_SIZE_MAX_PCDEV4,
+			.serial_number = "PCDEV4XYZ123",
+			.perm = RDWR 
+		}
+	}
 };
 
 loff_t pcd_lseek(struct file *filp, loff_t offset, int whence){
